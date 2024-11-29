157aa2376badbfca57e1a1951cf8694afd446bb9
alexandrudita <ditza_alex@yahoo.com>
2024-11-29 11:20:47 +0200

    Add files via upload

---
+++ diverse/xml/Product.java
@@ -0,0 +1,41 @@
+package eu.ase.ro.damapp.network.xml;
+
+import java.io.Serializable;
+
+public class Product implements Serializable {
+
+    private String name;
+    private Double price;
+
+    public Product() {
+    }
+
+    public Product(String name, Double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "name='" + name + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
